5a2ac710808604e24fb8d90b6a7fac20742b6884
Paulo Vinícius <vinicrf81@gmail.com>
2026-05-05 21:51:18 -0300

    Curva aprendizado

--- src/graficos.py
+++ src/graficos.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.metrics import PredictionErrorDisplay
 
 def plot_coefs(df_coeficientes):
     df_coeficientes.plot.barh()
@@ -7,3 +10,28 @@ def plot_coefs(df_coeficientes):
     plt.xlabel("Coeficiente")
     
     plt.gca().get_legend().remove()
+
+
+def plot_residuos(y_true, y_pred):
+    residuos = y_true - y_pred
+
+    fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+
+    h = sns.histplot(residuos, kde=True, ax=axs[0])
+
+    error_display_01 = PredictionErrorDisplay.from_predictions(
+    y_true=y_true,
+    y_pred=y_pred,
+    ax=axs[1]
+)
+    
+    error_display_02 = PredictionErrorDisplay.from_predictions(
+    y_true=y_true,
+    y_pred=y_pred,
+    kind='actual_vs_predicted',
+    ax=axs[2]
+)
+    
+    plt.tight_layout()
+
+    plt.show()
