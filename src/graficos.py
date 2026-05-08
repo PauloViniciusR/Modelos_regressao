fb5a66952b565450d3bad0eb28f62e86056c41a6
Paulo Vinícius <vinicrf81@gmail.com>
2026-05-08 19:01:20 -0300

    Regressao linear

--- src/graficos.py
+++ src/graficos.py
@@ -3,9 +3,10 @@ import seaborn as sns
 
 from sklearn.metrics import PredictionErrorDisplay
 
-def plot_coefs(df_coeficientes):
+def plot_coefs(df_coeficientes, titulo="Coeficientes"):
     df_coeficientes.plot.barh()
 
+    plt.title(titulo)
     plt.axvline(x=0, color="0.5")
     plt.xlabel("Coeficiente")
     
